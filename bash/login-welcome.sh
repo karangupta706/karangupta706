d7d5b3e3f54f0bd7e7e1552eb15648e9e258bd15
Karan <karanguptakg2000@gmail.com>
2020-02-05 14:25:36 -0800

    Lab 3 files done!@!

---
+++ bash/login-welcome.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# This script produces a dynamic welcome message
+# it should look like
+#   Welcome to planet hostname, title name!
+
+# Task 1: Use the variable $USER instead of the myname variable to get your name
+# Task 2: Dynamically generate the value for your hostname variable using the hostname command - e.g. $(hostname)
+# Task 3: Add the time and day of the week to the welcome message using the format shown below
+#   Use a format like this:
+#   It is weekday at HH:MM AM.
+# Task 4: Set the title using the day of the week
+#   e.g. On Monday it might be Optimist, Tuesday might be Realist, Wednesday might be Pessimist, etc.
+#   You will need multiple tests to set a title
+#   Invent your own titles, do not use the ones from this example
+
+###############
+# Variables   #
+###############
+name="$USER"
+date=$(date +'%I:%M %p')
+hostname=$(hostname)
+weekday=$(date +%u)
+###############
+# Main        #
+###############
+cat <<EOF
+
+cowsay "Welcome to planet $hostname, $name!"
+
+EOF
+if [ "$weekday" = "6" ] || [ "$weekday" = "7" ]
+then
+  echo "It is $date on weekend"
+else
+  echo "It is $date on weekday"
+fi
+display=$(cat <<EOF
+welcome to planet $hostname, $name
+EOF
+)
+cat <<EOF
+$(cowsay $display)
+
+EOF
